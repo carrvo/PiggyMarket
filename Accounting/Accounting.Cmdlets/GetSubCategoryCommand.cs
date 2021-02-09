04d7c4361c926d82c706e9943fdd270c33a78b4a
carrvo <carrvo@gmail.com>
2021-02-08 22:25:28 -0800

    Cmdlet organization
    Signed-off-by: carrvo <carrvo@gmail.com>

---
+++ Accounting/Accounting.Cmdlets/GetSubCategoryCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.Management.Service.Accounting.Cmdlets
+{
+    public sealed class GetSubCategoryCommand : Cmdlet
+    {
+    }
+}
