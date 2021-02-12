50b3f2233a0fc7f1548ec64ae78cc6f26ac90187
carrvo <carrvo@gmail.com>
2021-02-11 16:06:37 -0800

    utilities component
    Signed-off-by: carrvo <carrvo@gmail.com>

---
+++ Utilities/Utilities.Cmdlets/MigrateSchemaVersionCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.Management.Service.Utilities.Cmdlets
+{
+    public sealed class MigrateSchemaVersionCommand : Cmdlet
+    {
+    }
+}
