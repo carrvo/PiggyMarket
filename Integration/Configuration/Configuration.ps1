ab163b536ae956de4926c2514c17ed381bea82b1
carrvo <carrvo@gmail.com>
2021-02-28 16:36:22 -0800

    ThirdParty component
    Signed-off-by: carrvo <carrvo@gmail.com>

---
+++ Integration/Configuration/Configuration.ps1
@@ -0,0 +1,2 @@
+Import-Module $(Join-Path -Path $PSScriptRoot -ChildPath 'bin' -AdditionalChildPath 'net5.0','ThirdParty.Implementation.dll')
+Import-Module $(Join-Path -Path $PSScriptRoot -ChildPath 'bin' -AdditionalChildPath 'net5.0','ThirdParty.Cmdlets.dll')
